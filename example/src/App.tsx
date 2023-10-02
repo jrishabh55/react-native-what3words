ae53966ec1e6b0ed038b4105e9ad3ca919f9562f
Rishabh Jain <hello@rishabhjain.dev>
2023-10-03 00:09:40 +0530

    feat: separated location logic from component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 import { StyleSheet, Text, View } from 'react-native';
 import What3Words from 'react-native-what3words';
+import useLocation from './hooks/useLocation';
 
-const API_KEY = '';
-const MAP_API_KEY = '';
+const API_KEY = 'ICYFWDRW';
+const MAP_API_KEY = 'AIzaSyB0N8Lgj0Vgg5RV10COdLcanKH_9Ll0zhg';
 
 export default function App() {
+  const { isLoading, location } = useLocation();
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>What3Words</Text>
@@ -14,6 +25,8 @@ export default function App() {
         apiKey={API_KEY}
         mapApiKey={MAP_API_KEY}
         onEvent={console.log}
+        lat={location?.latitude ?? 0}
+        lng={location?.longitude ?? 0}
       />
     </View>
   );
--- src/components/What3Words.tsx
+++ src/components/What3Words.tsx
@@ -1,14 +1,14 @@
 import type { VFC } from 'react';
 import React, { useMemo } from 'react';
-import { Text } from 'react-native';
 import WebView, { type WebViewMessageEvent } from 'react-native-webview';
-import useLocation from '../useLocation';
 import { getWhat3WordsAsset } from '../utils';
 
 export type What3WordsProps = {
   onEvent?: (data: { type: string; data: any }) => void;
   apiKey: string;
   mapApiKey: string;
+  lat?: number;
+  lng?: number;
 };
 
 export const What3Words: VFC<What3WordsProps> = ({
@@ -17,8 +17,6 @@ export const What3Words: VFC<What3WordsProps> = ({
   onEvent,
   ...rest
 }) => {
-  const { location, isLoading } = useLocation();
-
   const onMessage = (payload: WebViewMessageEvent) => {
     try {
       const dataPayload = JSON.parse(payload.nativeEvent.data);
@@ -36,16 +34,14 @@ export const What3Words: VFC<What3WordsProps> = ({
     const _htmlContent = getWhat3WordsAsset({
       apiKey,
       mapApiKey,
-      lat: location?.latitude ?? 0,
-      lng: location?.longitude ?? 0,
       ...rest,
     });
     return _htmlContent;
-  }, [apiKey, location?.latitude, location?.longitude, mapApiKey, rest]);
-
-  if (isLoading) {
-    return <Text>Loading...</Text>;
-  }
+  }, [apiKey, mapApiKey, rest]);
+  console.log(
+    '🚀 ~ file: What3Words.tsx:41 ~ htmlContent ~ htmlContent:',
+    htmlContent
+  );
 
   return (
     <WebView
